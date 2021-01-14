462c42b2b2852b6f70dace088398f07d83bd1b8a
Jeb Griffin <jebaholic@gmail.com>
2021-01-14 13:24:53 -0600

    making new game

--- connect-four.js
+++ connect-four.js
@@ -1,1 +1,35 @@
-dd
+import { Game } from './game.js';
+let game = undefined;
+
+window.addEventListener("DOMContentLoaded", () => {
+
+const playerOneName = document.getElementById("player-1-name");
+const playerTwoName = document.getElementById("player-2-name");
+const newGameButton = document.getElementById("new-game");
+playerOneName.addEventListener("keyup", event => {
+  let playerOne = false;
+    // newGameButton.disabled = false;
+
+
+})
+
+playerTwoName.addEventListener("keyup", event => {
+//   newGameButton.disabled = false;
+   let
+   })
+
+if (playerOneName.value !== 0 && playerTwoName.value !== 0) {
+    newGameButton.disabled = false;
+} else {
+    newGameButton.disabled = true;
+}
+
+
+
+
+
+
+
+
+
+})
---
+++ game.js
@@ -0,0 +1,9 @@
+export class Game {
+    constructor(playerOne, playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+    getName(){
+        return `${this.playerOne} vs. ${this.playerTwo}`;
+    }
+}
