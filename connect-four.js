20584465668df2fabe1c755c8c78e3041566d2db
Jeb Griffin <jebaholic@gmail.com>
2021-01-14 14:47:30 -0600

    new Button

--- connect-four.js
+++ connect-four.js
@@ -6,23 +6,24 @@ window.addEventListener("DOMContentLoaded", () => {
 const playerOneName = document.getElementById("player-1-name");
 const playerTwoName = document.getElementById("player-2-name");
 const newGameButton = document.getElementById("new-game");
+
+// let playerOneNameValue = playerOneName.value;  // y this one doesn't work?
+// let playerTwoNameValue = playerTwoName.value;
 playerOneName.addEventListener("keyup", event => {
-  let playerOne = false;
-    // newGameButton.disabled = false;
-
-
-})
-
-playerTwoName.addEventListener("keyup", event => {
-//   newGameButton.disabled = false;
-   let
-   })
-
-if (playerOneName.value !== 0 && playerTwoName.value !== 0) {
+if (playerOneName.value && playerTwoName.value){
     newGameButton.disabled = false;
-} else {
+}else {
     newGameButton.disabled = true;
 }
+})
+playerTwoName.addEventListener("keyup", event => {
+    if (playerOneName.value && playerTwoName.value){
+        newGameButton.disabled = false;
+    }else {
+        newGameButton.disabled = true;
+    }
+   })
+
 
 
 
